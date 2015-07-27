23936f19639b3e56333b8aad3a89b190c2ba20d5
dbowens117 <dwaynebowens22@yahoo.com>
2015-07-27 18:44:15 -0500

    Quiz5
    Quiz5

---
+++ application.js
@@ -0,0 +1,16 @@
+ function Delta() { 
+ 	for(var i = 100; i >= 0; i=i-1) { 
+ 		if (i === 0) { 
+ 			document.write(finalmessage); 
+ 			} else { 
+ 			document.write("<p>Delta</p>"); 
+ 		} 
+ 	} 
+ function Sigma() { 
+ 	for(var i = 100; i >= 0; i=i-1) { 
+ 		if (i === 0) { 
+ 			document.write(finalmessage); 
+ 			} else { 
+ 			document.write("<p>Sigma</p>"); 
+ 		} 
+ 	} 
